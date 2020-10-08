90ad01f28cd311dbd1df87dabc033322fa9d6804
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-10-07 23:11:25 -0400

    Create Add_Border.js

---
+++ Add_Border.js
@@ -0,0 +1,29 @@
+
+/*
+  Given a rectangular matrix of characters, add a border of asterisks(*) to it.
+
+  Example
+  picture = ["abc", 
+             "ded"]
+  addBorder(picture) = ["*****",
+                        "*abc*",
+                        "*ded*",
+                        "*****"]
+*/
+
+function addBorder(picture) {
+  var border = '';
+  var length = picture[0].length+2;
+
+  for (var j = 0; j<picture.length; j++){
+    var added = '*'+picture[j]+'*';
+    picture[j] = added;
+  }
+
+  for (var i = 0; i<length; i++){
+    border += '*';
+  }
+  picture.splice(0,0,border);
+  picture.push(border);
+  return picture;
+}
